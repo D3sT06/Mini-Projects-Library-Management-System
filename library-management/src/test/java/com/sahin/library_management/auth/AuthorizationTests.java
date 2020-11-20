111aab85a33d458da9ccd978f0ad0ad2aa9cf625
Serkan <serkansahin27@gmail.com>
2020-11-20 10:08:00 +0300

    updated

--- library-management/src/test/java/com/sahin/library_management/auth/AuthorizationTests.java
+++ library-management/src/test/java/com/sahin/library_management/auth/AuthorizationTests.java
@@ -3,9 +3,12 @@ package com.sahin.library_management.auth;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sahin.library_management.LibraryManagementApp;
 import com.sahin.library_management.infra.enums.AccountFor;
+import com.sahin.library_management.infra.enums.LoginType;
+import com.sahin.library_management.infra.model.account.AccountLoginType;
 import com.sahin.library_management.infra.model.account.LibraryCard;
 import com.sahin.library_management.infra.model.auth.LoginModel;
 import com.sahin.library_management.infra.model.book.Author;
+import com.sahin.library_management.service.AccountLoginTypeService;
 import com.sahin.library_management.service.LibraryCardService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -23,10 +26,13 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.Instant;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -52,28 +58,38 @@ public class AuthorizationTests {
     @MockBean
     private LibraryCardService libraryCardService;
 
+    @MockBean
+    private AccountLoginTypeService accountLoginTypeService;
+
     @Nested
     @DisplayName("When we have valid token")
     class ValidToken {
         private LoginModel loginModel = new LoginModel();
         private LibraryCard validCard = new LibraryCard();
+        private AccountLoginType loginType = new AccountLoginType();
 
         @BeforeEach
         void setup() {
             loginModel.setBarcode(UUID.randomUUID().toString());
             loginModel.setPassword("1234");
 
+            loginType.setType(LoginType.PASSWORD);
+            loginType.setId(1L);
+            loginType.setLibraryCard(validCard);
+
             validCard.setAccountFor(AccountFor.MEMBER);
             validCard.setActive(true);
             validCard.setBarcode(loginModel.getBarcode());
             validCard.setIssuedAt(Instant.now().getEpochSecond());
             validCard.setPassword(passwordEncoder.encode(loginModel.getPassword()));
+            validCard.setLoginTypes(new HashSet<>(Arrays.asList(loginType)));
         }
 
         @Test
         @DisplayName("Then you can authorize if you have permission")
         void validAccess() throws Exception {
             given(libraryCardService.loadUserByUsername(any())).willReturn(validCard);
+            given(accountLoginTypeService.doesExist(anyString(), any())).willReturn(true);
 
             mockMvc
                     .perform(
@@ -97,6 +113,7 @@ public class AuthorizationTests {
         @DisplayName("Then you cannot authorize if you don't have permission")
         void invalidAccess() throws Exception {
             given(libraryCardService.loadUserByUsername(any())).willReturn(validCard);
+            given(accountLoginTypeService.doesExist(anyString(), any())).willReturn(true);
 
             mockMvc
                     .perform(
