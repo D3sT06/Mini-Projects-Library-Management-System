6eb1b87b6a35898f64f0f95ff9b62dbc1f5755fe
Serkan <dest_tr@hotmail.com>
2021-06-27 15:17:41 +0300

    update

---
+++ microservice-structure/lms-infra/target/generated-sources/annotations/com/sahin/lms/infra/mapper/BookReservingMapperImpl.java
@@ -0,0 +1,15 @@
+package com.sahin.lms.infra.mapper;
+
+import javax.annotation.Generated;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-06-27T15:17:23+0300",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 1.8.0_101 (Oracle Corporation)"
+)
+@Component
+@Primary
+public class BookReservingMapperImpl extends BookReservingMapperDecorator implements BookReservingMapper {
+}
