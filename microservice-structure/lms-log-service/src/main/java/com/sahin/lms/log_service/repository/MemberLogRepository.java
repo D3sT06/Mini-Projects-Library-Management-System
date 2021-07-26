c37f413b66a04a2b24e0ac3696e21fefde0a45f8
Serkan <dest_tr@hotmail.com>
2021-07-26 20:58:25 +0300

    update

--- microservice-structure/lms-log-service/src/main/java/com/sahin/lms/log_service/repository/MemberLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sahin.lms.log_service.repository;
-
-import com.sahin.lms.infra_entity.log.document.MemberLogEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MemberLogRepository extends MongoRepository<MemberLogEntity, String> {
-}
