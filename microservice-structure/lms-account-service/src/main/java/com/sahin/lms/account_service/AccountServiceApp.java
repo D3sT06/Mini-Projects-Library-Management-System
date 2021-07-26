a69a267ea6c64219038084c0878c31a995ea6775
Serkan <dest_tr@hotmail.com>
2021-07-26 21:11:15 +0300

    update

--- microservice-structure/lms-account-service/src/main/java/com/sahin/lms/account_service/AccountServiceApp.java
+++ microservice-structure/lms-account-service/src/main/java/com/sahin/lms/account_service/AccountServiceApp.java
@@ -3,12 +3,10 @@ package com.sahin.lms.account_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableEurekaClient
 @ComponentScan({"com.sahin.lms"})
 @EntityScan({"com.sahin.lms"})
 @EnableJpaRepositories({"com.sahin.lms"})
