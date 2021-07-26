bb0aa4a53d285b59c8aa554317707fc5f1eef3df
Serkan <dest_tr@hotmail.com>
2021-07-26 21:00:11 +0300

    update

--- microservice-structure/lms-loan-service/src/main/java/com/sahin/lms/loan_service/repository/BookItemStateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sahin.lms.loan_service.repository;
-
-import com.sahin.lms.infra_entity.loan.jpa.BookItemStateEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookItemStateRepository extends JpaRepository<BookItemStateEntity, String> {
-}
