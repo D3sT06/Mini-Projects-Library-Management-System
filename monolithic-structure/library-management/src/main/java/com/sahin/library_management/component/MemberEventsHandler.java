cc24e4dfcadc69feed6ce549e39fb30a527cd61c
Serkan <serkansahin27@gmail.com>
2021-07-27 09:19:03 +0300

    update

--- monolithic-structure/library-management/src/main/java/com/sahin/library_management/component/MemberEventsHandler.java
+++ monolithic-structure/library-management/src/main/java/com/sahin/library_management/component/MemberEventsHandler.java
@@ -2,16 +2,12 @@ package com.sahin.library_management.component;
 
 import com.sahin.library_management.config.RabbitMqConfig;
 import com.sahin.library_management.infra.model.log.MemberLog;
-import com.sahin.library_management.service.member_log.MemberLogService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
@@ -19,9 +15,6 @@ import java.util.concurrent.BlockingQueue;
 @ConditionalOnBean(RabbitMqConfig.class)
 @Slf4j
 public class MemberEventsHandler {
-
-    @Autowired
-    private MemberLogService memberLogService;
 
     private BlockingQueue<MemberLog> memberLogsQueue = new ArrayBlockingQueue<>(1000);
 
@@ -32,11 +25,6 @@ public class MemberEventsHandler {
 
     @Scheduled(fixedDelay = 10000)
     public void saveLogs() {
-        Collection<MemberLog> collection = new ArrayList<>(memberLogsQueue.size());
-
-        memberLogsQueue.drainTo(collection);
-        memberLogService.saveAll(collection);
-
-        log.info(collection.size() + " items has been saved to the mongo db");
+        log.info("Member logs queue size: " + memberLogsQueue.size());
     }
 }
--- monolithic-structure/library-management/src/main/java/com/sahin/library_management/repository/mongo/MemberLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sahin.library_management.repository.mongo;
-
-import com.sahin.library_management.infra.entity.document.MemberLogEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MemberLogRepository extends MongoRepository<MemberLogEntity, String> {
-}
