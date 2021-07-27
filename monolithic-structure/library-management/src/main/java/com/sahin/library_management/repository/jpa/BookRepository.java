8a0172f5d9d90a9e1c6126c332dab7a7597f9ffc
Serkan <serkansahin27@gmail.com>
2021-07-27 15:58:32 +0300

    update

--- monolithic-structure/library-management/src/main/java/com/sahin/library_management/repository/jpa/BookRepository.java
+++ monolithic-structure/library-management/src/main/java/com/sahin/library_management/repository/jpa/BookRepository.java
@@ -1,16 +1,9 @@
 package com.sahin.library_management.repository.jpa;
 
 import com.sahin.library_management.infra.entity.jpa.BookEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface BookRepository extends CrudRepository<BookEntity, Long> {
+public interface BookRepository extends JpaRepository<BookEntity, Long> {
 }
