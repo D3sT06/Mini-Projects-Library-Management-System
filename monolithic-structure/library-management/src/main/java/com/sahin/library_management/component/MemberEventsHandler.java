662c76d4462b10423ab77e0689ff7024368c625b
Serkan <serkansahin27@gmail.com>
2021-07-27 09:13:32 +0300

    update

--- monolithic-structure/library-management/src/main/java/com/sahin/library_management/component/MemberEventsHandler.java
+++ monolithic-structure/library-management/src/main/java/com/sahin/library_management/component/MemberEventsHandler.java
@@ -1,13 +1,7 @@
 package com.sahin.library_management.component;
 
 import com.sahin.library_management.config.RabbitMqConfig;
-import com.sahin.library_management.infra.enums.AccountFor;
-import com.sahin.library_management.infra.enums.LogAction;
-import com.sahin.library_management.infra.enums.QueryTerm;
-import com.sahin.library_management.infra.model.log.LogAggregation;
 import com.sahin.library_management.infra.model.log.MemberLog;
-import com.sahin.library_management.infra.model.log.MemberLogWithBarcodeAggregation;
-import com.sahin.library_management.service.LogAggregationService;
 import com.sahin.library_management.service.member_log.MemberLogService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -18,8 +12,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
@@ -30,9 +22,6 @@ public class MemberEventsHandler {
 
     @Autowired
     private MemberLogService memberLogService;
-
-    @Autowired
-    private LogAggregationService logAggregationService;
 
     private BlockingQueue<MemberLog> memberLogsQueue = new ArrayBlockingQueue<>(1000);
 
@@ -49,31 +38,5 @@ public class MemberEventsHandler {
         memberLogService.saveAll(collection);
 
         log.info(collection.size() + " items has been saved to the mongo db");
-    }
-
-    @Scheduled(fixedDelay = 10000)
-    public void saveAggregations() {
-
-        List<LogAggregation> aggregations = new LinkedList<>();
-
-        for(QueryTerm term : QueryTerm.values()) {
-            List<MemberLogWithBarcodeAggregation> events = memberLogService.getActionAggregations(term);
-
-            events.forEach(event -> {
-                LogAggregation logAggregation = new LogAggregation();
-                logAggregation.setAccountFor(AccountFor.MEMBER);
-                logAggregation.setAction(LogAction.valueOf(event.getGroup().getAction()));
-                logAggregation.setActionCount(event.getActionCount());
-                logAggregation.setBarcode(event.getGroup().getCardBarcode());
-                logAggregation.setQueryTerm(term);
-
-                aggregations.add(logAggregation);
-            });
-
-        }
-
-        logAggregationService.saveAll(aggregations);
-
-        log.info("Aggregations has been updated with " + aggregations.size() + " items");
     }
 }
