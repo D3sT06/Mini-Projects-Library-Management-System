71e6fa4270a791e1f842e77b8fd821e9d027664b
Serkan <serkansahin27@gmail.com>
2021-07-27 11:05:44 +0300

    update

--- monolithic-structure/library-management/src/main/java/com/sahin/library_management/infra/projections/AuthorProjections.java
+++ monolithic-structure/library-management/src/main/java/com/sahin/library_management/infra/projections/AuthorProjections.java
@@ -8,16 +8,14 @@ import java.util.List;
 
 public class AuthorProjections {
 
-    @JsonPropertyOrder({"id","bookIds"})
+    @JsonPropertyOrder({"id", "name", "surname", "bookIds"})
     public interface AuthorView {
         Long getId();
 
         @NotNull
-        @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
         String getName();
 
         @NotNull
-        @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
         String getSurname();
 
         @JsonIdentityInfo(
