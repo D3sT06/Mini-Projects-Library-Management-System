d8479ca5a02ce05b841e530b7f7cb4bf7fa2362f
Serkan <serkansahin27@gmail.com>
2021-07-27 15:57:55 +0300

    update

--- monolithic-structure/library-management/src/main/java/com/sahin/library_management/repository/jpa/BookRepository.java
+++ monolithic-structure/library-management/src/main/java/com/sahin/library_management/repository/jpa/BookRepository.java
@@ -1,9 +1,9 @@
 package com.sahin.library_management.repository.jpa;
 
 import com.sahin.library_management.infra.entity.jpa.BookEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository extends CrudRepository<BookEntity, Long> {
+public interface BookRepository extends JpaRepository<BookEntity, Long> {
 }
