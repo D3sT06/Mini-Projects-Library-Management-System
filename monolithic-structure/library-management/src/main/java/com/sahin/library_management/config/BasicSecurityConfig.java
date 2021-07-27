fb6b25d85e16acfa93d7b7c8a866ffcc5d9b2e10
Serkan <serkansahin27@gmail.com>
2021-07-27 11:14:35 +0300

    update

--- monolithic-structure/library-management/src/main/java/com/sahin/library_management/config/BasicSecurityConfig.java
+++ monolithic-structure/library-management/src/main/java/com/sahin/library_management/config/BasicSecurityConfig.java
@@ -3,12 +3,18 @@ package com.sahin.library_management.config;
 import com.sahin.library_management.infra.auth.MyBasicAuthenticationEntryPoint;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true
+)
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final String[] AUTH_WHITELIST = {
