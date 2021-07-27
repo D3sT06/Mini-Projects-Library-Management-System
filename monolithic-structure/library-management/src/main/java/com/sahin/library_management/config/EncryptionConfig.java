991ab9f5ed8c2c5b9b9af8ba4db25db9ebeae2ce
Serkan <serkansahin27@gmail.com>
2021-07-27 13:34:10 +0300

    update

--- monolithic-structure/library-management/src/main/java/com/sahin/library_management/config/EncryptionConfig.java
+++ monolithic-structure/library-management/src/main/java/com/sahin/library_management/config/EncryptionConfig.java
@@ -10,21 +10,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class EncryptionConfig {
 
     @Bean
-    public PasswordEncoder noEncoder() {
-        return new PasswordEncoder() {
-            @Override
-            public String encode(CharSequence rawPassword) {
-                return rawPassword.toString();
-            }
-
-            @Override
-            public boolean matches(CharSequence rawPassword, String encodedPassword) {
-                return rawPassword.toString().equals(encodedPassword);
-            }
-        };
-    }
-
-    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
