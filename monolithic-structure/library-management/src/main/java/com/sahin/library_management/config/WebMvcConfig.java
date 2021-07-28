b1d1d945771d0f3b3f49c4d8caad641447769798
Serkan <serkansahin27@gmail.com>
2021-07-28 13:41:56 +0300

    update

--- monolithic-structure/library-management/src/main/java/com/sahin/library_management/config/WebMvcConfig.java
+++ monolithic-structure/library-management/src/main/java/com/sahin/library_management/config/WebMvcConfig.java
@@ -1,8 +1,6 @@
 package com.sahin.library_management.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -19,10 +17,5 @@ public class WebMvcConfig implements WebMvcConfigurer {
         if (!registry.hasMappingForPattern("/**")) {
             registry.addResourceHandler("/**").addResourceLocations(STATIC_RESOURCE);
         }
-    }
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
     }
 }
