ef09cfb60619a8083a7fb30b9c5fa0cfbfb4bd24
Serkan <serkansahin27@gmail.com>
2021-07-28 12:59:03 +0300

    update

--- monolithic-structure/library-management/src/main/java/com/sahin/library_management/bootstrap/AccountLoader.java
+++ monolithic-structure/library-management/src/main/java/com/sahin/library_management/bootstrap/AccountLoader.java
@@ -22,31 +22,25 @@ public class AccountLoader implements Loader<MemberEntity> {
 
     private void loadMembers() {
 
-        MemberEntity librarianEntity1 = new MemberEntity();
-        librarianEntity1.setName("Serkan");
-        librarianEntity1.setSurname("Sahin");
-        librarianEntity1.setEmail("serkans@sahin.com");
+        MemberEntity entity1 = new MemberEntity();
+        entity1.setName("Serkan");
+        entity1.setSurname("Sahin");
+        entity1.setEmail("serkans@sahin.com");
 
-        MemberEntity librarianEntity2 = new MemberEntity();
-        librarianEntity2.setName("John");
-        librarianEntity2.setSurname("Doe");
-        librarianEntity2.setEmail("johndoe@sahin.com");
+        MemberEntity entity2 = new MemberEntity();
+        entity2.setName("John");
+        entity2.setSurname("Doe");
+        entity2.setEmail("johndoe@sahin.com");
 
-        MemberEntity librarianEntity3 = new MemberEntity();
-        librarianEntity3.setName("Alex");
-        librarianEntity3.setSurname("Watt");
-        librarianEntity3.setEmail("alexw@sahin.com");
+        MemberEntity entity3 = new MemberEntity();
+        entity3.setName("Alex");
+        entity3.setSurname("Watt");
+        entity3.setEmail("alexw@sahin.com");
 
 
-        libraryRepository.save(librarianEntity1);
-        libraryRepository.save(librarianEntity2);
-        libraryRepository.save(librarianEntity3);
-    }
-
-    @Override
-    public void clearDb() {
-        List<MemberEntity> memberEntities = this.getAll();
-        memberEntities.forEach(entity -> libraryRepository.deleteById(entity.getBarcode(), MemberEntity.class));
+        libraryRepository.save(entity1);
+        libraryRepository.save(entity2);
+        libraryRepository.save(entity3);
     }
 
     @Override
